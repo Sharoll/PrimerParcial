1495ea963dc27b012435d5e3417b00f9bbc76f8e
sharoll <syuliannaaraujo@unicesar.edu.co>
2020-10-06 10:39:06 -0500

    Confirmo

--- ClientApp/src/app/Ayuda/models/persona.ts
+++ ClientApp/src/app/Ayuda/models/persona.ts
@@ -1,2 +1,13 @@
 export class Persona {
+    identificacion: string;
+    nombre: string;
+    apellido: string
+    sexo: string;
+    edad: number;
+    departamento: string;
+    ciudad: string;
+    valorApoyo: number;
+    modalidadApoyo: string;
+    fecha: Date;
+
 }
